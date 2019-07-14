5e0f324dadd4415f0a70a61b8ff39ac512d89379
TheWitcher1991 <ashot.svazyan222@gmail.com>
2019-07-14 13:34:37 +0300

    огые

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+/* eslint-env node, mocha */
+
+// import $ from 'jquery';
+
+window.onload = function () {
+
+	'use strict';
+
+	const $page = $('html', 'body');
+
+	$(document).ready(function () {
+		
+	});
+};
---
+++ src/tools/js/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* eslint-env node, mocha */
+
+// import $ from 'jquery';
+
+window.onload = function () {
+
+	'use strict';
+
+	var $page = $('html', 'body');
+
+	$(document).ready(function () {});
+};
