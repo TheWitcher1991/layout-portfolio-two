7793ee41a2231c631502cfd70585b6b94ade0a0c
TheWitcher1991 <ashot.svazyan222@gmail.com>
2019-07-24 12:31:26 +0300

    just

--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,41 @@ const gulp         = require('gulp'),
       del          = require('del'),
       cache        = require('gulp-cache');
 
+const PATHS = {
+    sass: {
+        src: 'app/src/sass/**/*.sass',
+        dest: 'app/src/tools/css'
+    },
+    build: 'build',
+    browser: 'app',
+    js: {
+        src: 'app/src/tools/js/index.js',
+        libs: 'app/src/libs/libs.min.js',
+        all: 'app/src/tools/js/**/*.js',
+        build: 'build/static/script'
+    },
+    html: {
+        src: 'app/*.html',
+        build: 'build',
+    },
+    css: {
+        src: 'app/src/tools/css/index.min.css',
+        build: 'build/static/style'
+    },
+    font: {
+        src: 'app/src/font/**/*',
+        build: 'build/static/font'
+    }
+}
+
 // ! Таск для компиляции sass -> css
 gulp.task('sass', function () {
-    return gulp.src('app/src/sass/*.sass')
+    return gulp.src(PATHS.sass.src)
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
         .pipe(cssnano())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('app/src/tools/css/'))
+        .pipe(gulp.dest(PATHS.sass.dest))
         .pipe(browserSync.reload({stream: true}))
 });
 
@@ -26,43 +53,43 @@ gulp.task('sass', function () {
 gulp.task('browser-sync', function () {
     browserSync({
         server: {
-			baseDir: 'app'
+			baseDir: PATHS.browser
 		},
         notify: false
     });
 });
 
 gulp.task('scripts', function() {
-    return gulp.src('app/src/tools/js/index.js')
+    return gulp.src(PATHS.js.all)
         .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('code', function() {
-    return gulp.src('app/*.html')
+    return gulp.src(PATHS.html.src)
         .pipe(browserSync.reload({stream: true}))
 });
 
 // ! Таск для полного удаления ./build перед сборкой
 gulp.task('clean', async function() {
-	return del.sync('build');
+	return del.sync(PATHS.build);
 });
 
 // TODO: конвертирование выбранных файлов для сборки в ./build 
 gulp.task('prebuild', async function() {
-    let buildCss = gulp.src('./app/src/tools/css/index.min.css')
-        .pipe(gulp.dest('build/static/style'))
+    let buildCss = gulp.src(PATHS.css.src)
+        .pipe(gulp.dest(PATHS.css.build))
 
-    let buildFont = gulp.src('./app/src/font/**/*')
-        .pipe(gulp.dest('build/static/font'))
+    let buildFont = gulp.src(PATHS.font.src)
+        .pipe(gulp.dest(PATHS.font.build))
 
     let buildJs = gulp.src([
-        './app/src/tools/js/**/*.js',
-        './app/src/libs/libs.min.js'
+        PATHS.js.all,
+        PATHS.js.libs
     ])
-        .pipe(gulp.dest('build/static/script'))
+        .pipe(gulp.dest(PATHS.js.build))
     
-    var buildHtml = gulp.src('app/*.html')
-        .pipe(gulp.dest('build'));
+    var buildHtml = gulp.src(PATHS.html.src)
+        .pipe(gulp.dest(PATHS.html.build));
 });
 
 // ! Таск для очистки кеша
@@ -72,9 +99,9 @@ gulp.task('clear', function (callback) {
 
 // ! Таск для наблюдение -> компиляция -> перезагрузка сервера
 gulp.task('watch', function () {
-    gulp.watch('app/src/sass/*.sass', gulp.parallel('sass'));
-    gulp.watch('app/*.html', gulp.parallel('code'));
-    gulp.watch('app/src/tools/js/index.js', gulp.parallel('scripts'));
+    gulp.watch(PATHS.sass.src, gulp.parallel('sass'));
+    gulp.watch(PATHS.html.src, gulp.parallel('code'));
+    gulp.watch(PATHS.js.all, gulp.parallel('scripts'));
 });
 
 // ! Таск для для выполения task('watch')
